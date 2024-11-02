4f87c48ca9a64e17da39badc4410acf9dfd0d611
Sergei Zakharov <thesdzakharov@gmail.com>
2024-11-02 10:35:42 +0100

    Removed .. as a prebuilt system alias; added more git aliases.

--- aliases.zsh
+++ aliases.zsh
@@ -1,9 +1,11 @@
 # System aliases
 alias upgrade="sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y"
-alias ..="cd .."
 
 # Git aliases
 alias gs='git status'
 alias gr='git restore'
 alias gl='git l'
 alias gg='git pull'
+alias ga='git add'
+alias gp='git push'
+alias gc='git commit'
