b3b607c0a01e4547525b4888b6ab356e83041b90
Sergei Zakharov <thesdzakharov@gmail.com>
2024-11-23 13:43:37 +0100

    Improved pyenv setup script.

--- pyenv_setup.zsh
+++ pyenv_setup.zsh
@@ -1,5 +1,8 @@
+export PATH="$HOME/.pyenv/bin:$PATH"
+eval "$(pyenv init --path)"
 eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
 export CERT_PATH=$(python3 -m certifi)
 export SSL_CERT_FILE=$CERT_PATH
 export REQUESTS_CA_BUNDLE=$CERT_PATH
-
