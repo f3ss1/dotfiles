2ac80d0b3a7789360613a647b638bd3491c26246
Sergei Zakharov <thesdzakharov@gmail.com>
2026-04-05 11:41:48 +0300

    Added omz installation to startup script.

--- startup.sh
+++ startup.sh
@@ -11,6 +11,9 @@ echo "export GIT_EMAIL=\"$GIT_EMAIL\"" >> $HOME/.private_env_variables.zsh
 curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+# Install OMZ
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # Download non-shipped omz plugins.
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
