6fef453210f1f4082184100be01986dafd61e1ce
Sergei Zakharov <thesdzakharov@gmail.com>
2026-06-11 17:45:47 +0200

    Added silence for homebrew.

--- zsh/.config/zsh/env_variables.zsh
+++ zsh/.config/zsh/env_variables.zsh
@@ -1,3 +1,6 @@
 ## MISC ##
 # (macOS-only) Prevent Homebrew from reporting - https://github.com/Homebrew/brew/blob/master/docs/Analytics.md
 export HOMEBREW_NO_ANALYTICS=1
+
+# Drop notification about homebrew updates
+export HOMEBREW_NO_ENV_HINTS=1
